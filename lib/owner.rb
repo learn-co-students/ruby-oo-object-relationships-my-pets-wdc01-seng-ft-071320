616024e90c13e28874cc66dd1d04602d4ea20634
Rbascoe <roanbascoe5@gmail.com>
2020-07-16 14:43:36 -0400

    owner file complete

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+@@all_owners = []
+
+  def initialize(name, species= "human")
+    @name = name
+    @species = species
+    @@all_owners << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@all_owners.length
+  end
+  
+  def self.reset_all
+    @@all_owners.clear
+  end
 end
