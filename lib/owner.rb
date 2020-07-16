0baacb03cd1e33e34fd41bb6fd2422be92d27051
github username <github email address>
2020-07-16 16:31:31 -0400

    some changes

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all_owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_owners << self
+  
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@all_owners.length
+  end
+
+  def self.reset_all
+    @@all_owners = []
+  end
+
+  def cats 
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+def walk_dogs
+  self.dogs.mood = "happy"
 end
+
+  
+
+
+
+
+
+
+
+
+
+end
+
+
---
+++ lib/run.rb
@@ -0,0 +1,6 @@
+require_relative "./cat.rb"
+require_relative "./dog.rb"
+require_relative "./owner.rb"
+require "pry"
+
+
